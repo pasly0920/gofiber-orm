d7577aa4e5741860d0626e4d9f90c26b3485c4a6
pasly0920 <inwoo920621@gmail.com>
2023-03-07 15:34:28 +0900

    feat: user App 추가

--- main.go
+++ main.go
@@ -1,13 +1,50 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	recover2 "github.com/gofiber/fiber/v2/middleware/recover"
+	"gofiber-orm/app/http/handler"
+	"gofiber-orm/app/http/routes"
+	"gofiber-orm/domain/repository"
+	"gofiber-orm/domain/service"
+	"gofiber-orm/infrastructure/database"
+	"gofiber-orm/infrastructure/logger"
+	"log"
+)
 
 func main() {
 	app := fiber.New()
 
+	// Initialize logger
+	logger := logger.NewLogger()
+
+	// Set up middleware
+	app.Use(logger)
+	app.Use(recover2.New())
+	app.Use(cors.New())
+
+	//initialize db
+	db, err := database.NewDB()
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	//Initialize repositories
+	userRepository := repository.NewUserRepository(db)
+
+	// Initialize services
+	userSvc := service.NewUserService(userRepository)
+
+	// Initialize handlers
+	userHandler := handler.NewUserHandler(userSvc, logger)
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+		return c.SendString("")
 	})
+
+	api := app.Group("/api")
+	routes.UserRouter(api, userHandler)
 
 	app.Listen(":8081")
 }
